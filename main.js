99a7b4a1a7ab94479c43d67b53a1ad14c56cc059
Elias Dorneles <eliasdorneles@gmail.com>
2020-05-02 10:46:25 +0200

    tell user when they got the correct answer

--- main.js
+++ main.js
@@ -76,14 +76,13 @@ const displayEqualization = (svgDocument, equalization) => {
     const heightBandFilter = 90
     return positionY0 - level * (heightBandFilter / 2 / 12) - 5
   }
-  equalization.forEach((level, index) => {
-    const sliderKnob = svgDocument.getElementById(`band-filter-${index + 1}`)
+  Array.from(svgDocument.getElementsByClassName("band-filter")).forEach((sliderKnob, index) => {
+    const level = equalization[index]
     sliderKnob.setAttribute("y", calcSliderPosition(level).toString())
     // here we add the level as data attribute to the slider, so that we can later recover
     sliderKnob.setAttribute("data-level", level)
   })
 }
-
 const getEqualizationFromSvgDocument = (svgDocument) =>
   Array.from(svgDocument.getElementsByClassName("band-filter")).map((x) =>
     parseInt(x.dataset.level),
@@ -97,6 +96,32 @@ const newGame = () => {
   return {
     currentEqualization: equalizationBase,
     quizEqualizationOptions: alternatives,
+  }
+}
+
+// EVENT ADDING/REMOVING HELPERS (adapted from: https://stackoverflow.com/a/4386514/149872)
+const _eventHandlers = {}
+
+const addListener = (node, event, handler, capture = false) => {
+  if (!(node in _eventHandlers)) {
+    _eventHandlers[node] = {}
+  }
+  if (!(event in _eventHandlers[node])) {
+    _eventHandlers[node][event] = []
+  }
+  _eventHandlers[node][event].push([handler, capture])
+  node.addEventListener(event, handler, capture)
+}
+
+const removeAllListeners = (node, event) => {
+  if (node in _eventHandlers) {
+    if (event in _eventHandlers[node]) {
+      for (let i = _eventHandlers[node][event].length; i--; ) {
+        const handler = _eventHandlers[node][event][i]
+        node.removeEventListener(event, handler[0], handler[1])
+      }
+      _eventHandlers[node][event] = []
+    }
   }
 }
 
@@ -116,9 +141,8 @@ const eqReset = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 let game = newGame()
 
 // UI stuff
-audioFileSelector.onchange = function() {
-  // this needs to be written with function() syntax in order to have this
-  const file = this.files[0]
+audioFileSelector.onchange = () => {
+  const file = audioFileSelector.files[0]
   equalizedAudioPlayer.src = URL.createObjectURL(file)
   uiAudioSourceName.textContent = file.name
   audioFileSelector.classList.add("d-none")
@@ -140,22 +164,43 @@ const handleClickButtonChangeAudio = () => {
   audioFileSelector.classList.remove("d-none")
 }
 
+const setUIMessage = (message) => (quizUIMessage.innerHTML = message)
+
 const handleClickSvgDocument = (svgDocument, index) => {
   const eqClicked = getEqualizationFromSvgDocument(svgDocument)
-  console.log("you clicked position", index)
   if (isEqualizationEqual(game.currentEqualization, eqClicked)) {
-    console.log("Congrats! You did it!", eqClicked)
+    setUIMessage('<strong class="text-success">Congrats! You did it!</strong>')
+    svgObjects.forEach((obj, i) => {
+      removeAllListeners(obj.getSVGDocument(), "click")
+      removeClickableLook(i)
+      if (i !== index) {
+        obj.classList.add("d-none")
+      }
+    })
   } else {
-    console.log("Oops! That's not it! Try again")
+    setUIMessage("<span class='text-danger'>Oops! That's not it! Try again</span>")
   }
+}
+
+// The equalizer is made to look clickable relying on CSS classes for the
+// <object> element to add border and :hover,  and the cursor is made "pointer"
+// with a CSS class for a transparent <rect /> put in the SVG for this specific purpose
+const addClickableLook = (index) => {
+  svgObjects[index].classList.add("svg-equalizer-clickable")
+  svgDocument = svgObjects[index].getSVGDocument()
+  svgDocument.getElementById("clickable-mask").classList.add("clickable")
+}
+const removeClickableLook = (index) => {
+  svgObjects[index].classList.remove("svg-equalizer-clickable")
+  svgDocument = svgObjects[index].getSVGDocument()
+  svgDocument.getElementById("clickable-mask").classList.remove("clickable")
 }
 
 const initSvgDocument = (svgDocument, index) => {
   const equalization = game.quizEqualizationOptions[index]
   displayEqualization(svgDocument, equalization)
-  svgDocument.addEventListener("click", function() {
-    handleClickSvgDocument(this, index)
-  })
+  addClickableLook(index)
+  addListener(svgDocument, "click", () => handleClickSvgDocument(svgDocument, index))
 }
 
 const svgObjects = Array.from(document.getElementsByTagName("object"))
